c71a381623729846aa4ecb9701a40bf5f0c7f924
Jose Castro Matos <josedavidcastro@unicesar.edu.co>
2026-03-31 15:25:52 -0500

    Feat: Creacion de la clase Proveedor y sus atributos en el paquete model

---
+++ src/main/java/com/sistemafarmacia/sistemafarmacia/model/Proveedor.java
@@ -0,0 +1,59 @@
+package com.sistemafarmacia.sistemafarmacia.model;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import java.util.List;
+
+@Entity
+public class Proveedor {
+  
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "El nit del proveedor es obligatorio")
+    private String nit;
+
+    @NotBlank(message = "El nombre del proveedor es obligatorio")
+    private String nombre;
+
+    @NotBlank(message = "El teléfono del proveedor es obligatorio")
+    private String telefono;
+
+    @NotBlank(message = "La dirección del proveedor es obligatoria")
+    private String direccion;
+
+    @NotBlank(message = "El correo electrónico del proveedor es obligatorio")
+    private String correoElectronico;
+
+    @OneToMany(mappedBy = "proveedor")
+    private List<Medicamento> medicamentos;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNit() {
+        return nit;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public String getCorreoElectronico() {
+        return correoElectronico;
+    }
+
+    public List<Medicamento> getMedicamentos() {
+        return medicamentos;
+    }
+
+}
