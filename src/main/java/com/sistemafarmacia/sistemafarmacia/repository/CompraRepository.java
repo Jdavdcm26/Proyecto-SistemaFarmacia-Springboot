3e90de5de933a5e62d1795fb48f160f02a460c2f
Jose Castro Matos <josedavidcastro@unicesar.edu.co>
2026-03-31 22:25:31 -0500

    Feat: Creacion de CompraRepository con extends JpaRepository en el paquete repository

---
+++ src/main/java/com/sistemafarmacia/sistemafarmacia/repository/CompraRepository.java
@@ -0,0 +1,9 @@
+package com.sistemafarmacia.sistemafarmacia.repository;
+
+import com.sistemafarmacia.sistemafarmacia.model.Compra;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface CompraRepository extends JpaRepository<Compra, Long> {
+    List<Compra> findByProveedorId(Long proveedorId);
+}
