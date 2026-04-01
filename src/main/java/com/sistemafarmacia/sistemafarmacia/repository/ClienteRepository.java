94286820e7b23dc895cf2dde2fa4cf27179dec6c
Jose Castro Matos <josedavidcastro@unicesar.edu.co>
2026-03-31 22:03:49 -0500

    Feat: Creacion de ClienteRepository con extends JpaRepository en el paquete repository

---
+++ src/main/java/com/sistemafarmacia/sistemafarmacia/repository/ClienteRepository.java
@@ -0,0 +1,8 @@
+package com.sistemafarmacia.sistemafarmacia.repository;
+
+import com.sistemafarmacia.sistemafarmacia.model.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;   
+import java.util.List;
+public interface ClienteRepository extends JpaRepository<Cliente, String> {
+    List<Cliente> findByIdentificacionContainingIgnoreCase(String identificacion);
+}
